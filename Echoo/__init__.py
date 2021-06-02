6371020c4ead6e4bb1bc17299a4cfe73050200f3
Ligeng Zhu <ligeng@mit.edu>
2021-06-02 18:50:39 +0800

    add class support and escapte mode

---
+++ Echoo/__init__.py
@@ -0,0 +1,1 @@
+from .echoo import Echoo
--- Echoo/echoo.py
+++ Echoo/echoo.py
@@ -4,15 +4,56 @@
 
 import os
 import argparse
-
+import codecs
 import telegram
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
+def escape_fn(s):
+    return s.replace("_", "\\_") \
+            .replace("*", "\\*") \
+            .replace("[", "\\[") \
+            .replace("`", "\\`") \
+            .replace("<", "\\<") \
+            .replace(">", "\\>")
+
+class Echoo:
+    def __init__(self, token=None, chat_id=None, parse_mode="MarkdownV2"):
+        print("Init from __init__")
+        self.token = token
+        if token is None:
+            try:
+                self.token = os.environ["TG_TOKEN"]
+            except KeyError:
+                raise KeyError("Neither --token nor TG_TOKEN is set.")
+        
+        self.chat_id = chat_id
+        if chat_id is None:
+            try:
+                self.chat_id = os.environ["TG_CHAT_ID"]
+            except KeyError:
+                raise KeyError("Neither --chat_id nor TG_CHAT_ID is set.")
+        self.bot = telegram.Bot(token=self.token)
+        # self.parse_mode = "Markdown"
+        self.parse_mode = parse_mode
+    
+    def send_msg(self, msg, chat_id=None):
+        if chat_id is None:
+            chat_id = self.chat_id
+        print(f"===== Echo: {msg} =====")
+        self.bot.send_message(chat_id=chat_id, text=msg, parse_mode=self.parse_mode)    
+    
+    def __call__(self, function):
+        print("Init from __call__")
+        def wrapper( *args, **kwds):
+            self.send_msg(escape_fn(f'''Start {str(function)}'''))
+            res = function(*args, **kwds)
+            self.send_msg(escape_fn(f'''Finish {str(function)}'''))
+            return res
+        return wrapper
 
 def main(token, chat_id, msg="Are u ok", parse_mode="Markdown"):
     bot = telegram.Bot(token=token)
-    bot.send_message(chat_id=chat_id, text=msg, parse_mode=parse_mode)
-
+    bot.send_message(chat_id=chat_id, text=escape_fn(msg), parse_mode=parse_mode)
 
 def run():
     parser = argparse.ArgumentParser(description=r'''Echoo: A tool let's your program echo to Telegram.''')
