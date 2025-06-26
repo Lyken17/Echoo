52de5866df33ff7d423b148498d726fd4bda0ddf
Ligeng Zhu <Lyken17@users.noreply.github.com>
2025-06-26 11:49:12 +0800

    Refactor main function in echoo.py to streamline token and chat_id handling, replacing individual checks with assertions and improving argument parsing for better clarity.

--- Echoo/echoo.py
+++ Echoo/echoo.py
@@ -64,19 +64,10 @@ class Echoo:
         return wrapper
 
 def main(msg="hello world", token=None, chat_id=None, parse_mode="MarkdownV2", no_escape=False, reply_to_message_id=None):
-    if token is None:
-        try:
-            token = os.environ["TG_TOKEN"]
-        except KeyError:
-            raise KeyError("Neither --token nor TG_TOKEN is set.")
-
-    if chat_id is None:
-        try:
-            chat_id = os.environ["TG_CHAT_ID"]
-        except KeyError:
-            raise KeyError("Neither --chat_id nor TG_CHAT_ID is set.")
+    assert token is not None and chat_id is not None, "Neither --token nor TG_TOKEN is set. Neither --chat_id nor TG_CHAT_ID is set."
     
     bot = telegram.Bot(token=token)
+    print(msg if no_escape else escape_fn(msg)); input()
     msg_info = asyncio.run(
         bot.send_message(chat_id=chat_id, text=msg if no_escape else escape_fn(msg), 
                          parse_mode=parse_mode, reply_to_message_id=reply_to_message_id,
@@ -87,29 +78,24 @@ def main(msg="hello world", token=None, chat_id=None, parse_mode="MarkdownV2", n
 def run():
     parser = argparse.ArgumentParser(description=r'''Echoo: A tool let's your program echo to Telegram.''')
     parser.add_argument("msg", default="Are u ok?", type=str, help="Message to send")
+    parser.add_argument("-t", "--token", default=None, type=str, help="Token for your bot.")
+    parser.add_argument("-id", "--chat_id", default=None, type=str, help="Chat_id of your audience.")
     args, unknown = parser.parse_known_args()
 
-    token = os.environ.get("TG_TOKEN", None)
-    chat_id = os.environ.get("TG_CHAT_ID", None)
+    token = os.environ.get("TG_TOKEN", args.token)
+    chat_id = os.environ.get("TG_CHAT_ID", args.chat_id)
     if args.msg and (token is None or chat_id is None):
         warnings.warn("Echoo to telegram will not work. Neither --token nor TG_TOKEN is set. Neither --chat_id nor TG_CHAT_ID is set.")
         print(f"[Echoo]" + f"{args.msg}")
         return 0
 
-    parser.add_argument("-t", "--token", default=None, type=str, help="Token for your bot.")
-    parser.add_argument("-id", "--chat_id", default=None, type=str, help="Chat_id of your audience.")
     parser.add_argument("--parse-mode", default="MarkdownV2", type=str, help="Send Markdown or HTML, if you want Telegram apps to show bold, italic, fixed-width text or inline URLs in your bot's message")
     parser.add_argument("-rid", "--reply-to-id", default=None, type=int, help="Message ID to reply to.")
     parser.add_argument("--return-id", action="store_true")
 
     args = parser.parse_args()
-    if args.token is None:
-        args.token = os.environ.get("TG_TOKEN", None)
 
-    if args.chat_id is None:
-        args.chat_id = os.environ.get("TG_CHAT_ID", None)
-
-    tid = main(token=args.token, chat_id=args.chat_id, msg=args.msg, 
+    tid = main(token=token, chat_id=chat_id, msg=args.msg, 
               parse_mode=args.parse_mode, reply_to_message_id=args.reply_to_id)
     
     if args.return_id:
